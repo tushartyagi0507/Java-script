a0cf1b82d5dc53588d732fc84b3ded690df81d74
Tushar Tyagi <tushartyagi@Tushars-MacBook-Air.local>
2024-07-02 19:01:03 +0530

    making big changes

--- Chapter01/test.js
+++
@@ -1,9 +0,0 @@
-/* JS is synchronous single threaded language which is dynamically types ( that is the type checking does 
-    not happen during compilation while in statically typed language the type checking happens during 
-    compile time only) 
-
-    single threaded means : executes one line at a time */
-
-
-console.log("Hello World");
-console.log("Tushar Tyagi");
--- javascript/basic.js
+++
@@ -1,8 +0,0 @@
-console.log("Hello World")
-let a = "5"
-console.log(a)
-let b = "Tushar"
-console.log(b)
-// console.log(typeof a);
-let c = Number(a)
-console.log(typeof c);
